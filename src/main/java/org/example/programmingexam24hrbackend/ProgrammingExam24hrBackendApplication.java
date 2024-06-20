b3385b0a237eecfc66b6c3e4faef1fe72c2dd15c
Magnus Carstensen <brondby_1964@hotmail.com>
2024-06-20 08:46:55 +0200

    opsætning af backend

---
+++ src/main/java/org/example/programmingexam24hrbackend/ProgrammingExam24hrBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.programmingexam24hrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingExam24hrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingExam24hrBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/programmingexam24hrbackend/ProgrammingExam24hrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.programmingexam24hrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingExam24hrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
